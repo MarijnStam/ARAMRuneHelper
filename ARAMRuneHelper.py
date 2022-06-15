563c1d9603c5ce68ad003c3f3d7e0920fe40b5d0
Marijn Stam <marijnstam@betonic.nl>
2022-06-15 15:19:32 +0200

    lcu: add a listener when a champion is locked, print the name

---
+++ ARAMRuneHelper.py
@@ -0,0 +1,35 @@
+import willump
+import json
+import asyncio
+
+async def newChampion(data):
+
+    #Iterate the JSON file to match the received ID to the Champion name
+    for champs in champions['data']:
+        if int(champions['data'][champs]['key']) is int(data['data']):
+            print(champions['data'][champs]['id'])
+
+async def main():
+    global wlp 
+    global champions
+
+    # Start the Willump module for interfacing with the League Client
+    wlp = await willump.start()
+
+    # Open the JSON file with all the Champion : ID information
+    f = open('champions.json', 'r', encoding='utf-8')
+    champions = json.load(f)
+	
+    # Subscribe to the "current champion" event, which is triggered everytime a new champion is locked in
+    championSubscription = await wlp.subscribe('OnJsonApiEvent_lol-champ-select_v1_current-champion', default_handler=newChampion)
+
+    while True:
+        await asyncio.sleep(10)
+
+if __name__ == '__main__':
+    loop = asyncio.get_event_loop()
+    try:
+        loop.run_until_complete(main())
+    except KeyboardInterrupt:
+        loop.run_until_complete(wlp.close())
+
